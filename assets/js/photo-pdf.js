477753adb017e75db73c966d1661d3383b2ae120
lutfiagustamel <lutfyagustamel.la@gmail.com>
2024-01-27 23:15:12 +0700

    perbaikan menu photo pdf

--- assets/js/photo-pdf.js
+++ assets/js/photo-pdf.js
@@ -9,21 +9,25 @@ async function convertToPDF() {
         resultText.innerText = 'Converting...'; // Menampilkan teks "Converting..." saat konversi sedang berlangsung
         loadingAnimation.style.display = 'inline-block'; // Menampilkan animasi loading
 
-        const reader = new FileReader();
-        reader.readAsArrayBuffer(file);
-        reader.onload = async function (event) {
-            const arrayBuffer = event.target.result;
-            const pdfData = await generatePDF(arrayBuffer);
-            const pdfObject = `<object width="100%" height="500" data="${pdfData}" type="application/pdf"></object>`;
-            pdfContainer.innerHTML = pdfObject;
-            resultText.innerText = 'Result'; // Menampilkan teks "Result" setelah konversi selesai
-            loadingAnimation.style.display = 'none'; // Sembunyikan animasi loading setelah konversi selesai
-            loadingAnimation.style.animation = 'none'; // Hentikan animasi loading
-        };
+        setTimeout(async function () {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(file);
+            reader.onload = async function (event) {
+                const arrayBuffer = event.target.result;
+                const pdfData = await generatePDF(arrayBuffer);
+                const pdfObject = `<object width="100%" height="500" data="${pdfData}" type="application/pdf"></object>`;
+                pdfContainer.style.display = "block";
+                pdfContainer.innerHTML = pdfObject;
+                resultText.style.display = "block";
+                resultText.innerText = 'Result'; // Menampilkan teks "Result" setelah konversi selesai
+                loadingAnimation.style.display = 'none'; // Sembunyikan animasi loading setelah konversi selesai
+            };
+        }, 2000); // Menunda eksekusi fungsi berikutnya selama 3 detik (3000 milidetik)
     } else {
         alert('Please select an image file.');
     }
 }
+
 
 async function generatePDF(arrayBuffer) {
     const { PDFDocument, degrees } = PDFLib; // Perhatikan bahwa kami menggunakan PDFLib di sini
@@ -42,4 +46,9 @@ async function generatePDF(arrayBuffer) {
 
     const pdfBytes = await pdfDoc.save();
     return URL.createObjectURL(new Blob([pdfBytes], { type: 'application/pdf' }));
+}
+
+async function cancelPdf() {
+    document.getElementById("pdfContainer").style.display = "none";
+    document.getElementById("resultText").style.display = "none";
 }
